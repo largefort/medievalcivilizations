f82ab6691f52ffa79f6eb0b05cd7c4cc278abcef
jafetegill17 <106451813+largefort@users.noreply.github.com>
2024-07-31 13:33:02 +0000

    Update versionhistory.js

--- versionhistory.js
+++ versionhistory.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', (event) => {
   const versionHistory = [
+    { version: "1.0.4", date: "July 6, 2024", description: "hi there i recently balanced the passive income function via script.js and i have balanced and a litle bit of nerfing with the unit income rates enjoy" },
    { version: "1.0.4", date: "July 6, 2024", description: "removed Medieval AI because it caused some issue that made players annoyed when playing medievalcivilizations.online because the chatbot always popsup during launch" },
     { version: "1.0.3", date: "July 6, 2024", description: "i have implemented cost increase on each every medieval militant units per purchase increases cost by 15%" },
     { version: "1.0.2", date: "July 6, 2024", description: "updated script.js to implement the four new medieval military units to be part of the game" },
